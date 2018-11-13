e389b5d9cc6dd7aac2a2fd83f5ce8ad766dabb7f
yts <sky877kr@gmail.com>
2018-11-13 15:14:51 +0900

    인트로

---
+++ app/src/main/java/com/yts/tsletter/ui/activity/IntroActivity.java
@@ -0,0 +1,71 @@
+package com.yts.tsletter.ui.activity;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+
+
+import com.yts.tsletter.BaseActivity;
+import com.yts.tsletter.R;
+import com.yts.tsletter.databinding.IntroBinding;
+
+import java.util.concurrent.TimeUnit;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+
+public class IntroActivity extends BaseActivity {
+    private CompositeDisposable mCompositeDisposable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        IntroBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_intro);
+
+        /*drawableAnimation(binding.imgCreate.getDrawable());*/
+
+        mCompositeDisposable = new CompositeDisposable();
+        mCompositeDisposable.add(Single.timer(1800, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        finish();
+                        Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+                }));
+    }
+
+    public void drawableAnimation(Drawable drawable) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (drawable instanceof AnimatedVectorDrawable) {
+                AnimatedVectorDrawable animatedVectorDrawable = (AnimatedVectorDrawable) drawable;
+                animatedVectorDrawable.start();
+            }
+        } else {
+            if (drawable instanceof AnimatedVectorDrawableCompat) {
+                AnimatedVectorDrawableCompat animatedVectorDrawableCompat = (AnimatedVectorDrawableCompat) drawable;
+                animatedVectorDrawableCompat.start();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mCompositeDisposable != null) {
+            mCompositeDisposable.dispose();
+        }
+    }
+}
